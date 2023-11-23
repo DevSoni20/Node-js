e04d7d187cc8a1f76764458c510c91c13e7177b1
devsoni-20 <devsoni4545@gmail.com>
2023-11-23 10:56:00 +0530

    Adding new file data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    {name:'Dev', email:'devsoni4545@gmail.com'},
+    {name:'Ragini', email:'myselfraginisingh@gmail.com'},
+    {name:'Ajay', email:'djmishra@gmail.com'}
+]
+module.exports = data;
